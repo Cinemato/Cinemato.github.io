2bde76d23a88ef647c18c156791a9f590d9b876b
Cinemato <alharerekhaled@gmail.com>
2022-02-01 01:30:47 +0300

    Mobile

--- Scripts/ordersMenu.js
+++ Scripts/ordersMenu.js
@@ -8,7 +8,21 @@ var originalHeight = orders.clientHeight;
 orders.style.height = "0px";
 
 function ordersToggle(){
-    if(screen.width > 800){
+   
+    if(screen.width <= 950 && screen.width > 800){
+        if(orders.style.height == "0px"){
+            orders.style.height = originalHeight  + "px";
+            container.style.width = "80%";
+            container.style.padding = "20px";
+        }
+        
+        else{
+            orders.style.height = "0px";
+            container.style.width = "70%";
+            container.style.padding = "10px";
+        }
+    }
+    else if(screen.width >= 800){
         if(orders.style.height == "0px"){
             orders.style.height = originalHeight  + "px";
             container.style.width = "50%";
